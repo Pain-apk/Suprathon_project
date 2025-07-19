f37be73bc1e28e21fa4792347d55ca3993463c44
Aditya Singh <adi465996@gmail.com>
2025-07-19 12:10:22 +0000

    added gitignore

--- back-end/server.js
+++ back-end/server.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const resumeController = require('../controllers/resumeController');
-
-// POST: Submit user data and generate resume
 router.post('/generate', resumeController.generateResume);
 
 module.exports = router;
